af0cedbba70c8b6a3b1c8e3e78e056cfa6d109ab
Gabriel Mol <96446469+GabrielVMol@users.noreply.github.com>
2023-10-08 23:42:33 -0300

    Initial commit

---
+++ src/main/java/br/com/gabriel/api/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package br.com.gabriel.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.gabriel.api.model.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+	
+}
